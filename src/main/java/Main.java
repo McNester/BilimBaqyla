31ee5a80c629864c3ebd3d8e012a49bbecbeaab8
McNester <seraf5@bk.ru>
2024-02-20 18:39:55 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import services.DataBaseConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBaseConnection db = new DataBaseConnection();
+
+        ResultSet rs = db.get("SELECT * FROM admins;\n");
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("adminId");
+                String username = rs.getString("username");
+                String passwd = rs.getString("passwd");
+
+                System.out.println();
+                System.out.println("Admin number "+id+":\n"+"UserName: "+ username+"\nPassword: "+passwd);
+                System.out.println();
+            }
+        }catch (SQLException throwables){
+            System.out.println(throwables.getMessage());
+        }
+    }
+}
---
+++ src/main/java/services/DataBaseConnection.java
@@ -0,0 +1,61 @@
+package services;
+
+
+import java.sql.*;
+
+public class DataBaseConnection {
+    private final String POSTGRES_HOST="ep-silent-scene-a4cd772l-pooler.us-east-1.aws.neon.tech";
+    private final String PORT = "5432";
+    private final String POSTGRES_DATABASE="verceldb";
+    private final String connectionString = "jdbc:postgresql://"+POSTGRES_HOST+":"+PORT+"/"+POSTGRES_DATABASE;
+    private final String user = "default";
+    private final String passwd = "R89LWSkZmQXt";
+    private Connection con = null;
+
+    public ResultSet get(String sqlQuery) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(connectionString,user,passwd);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlQuery);
+            return rs;
+        } catch (SQLException e) {
+            System.out.println("connection error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("driver error: " + e.getMessage());
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("could not close the connection: " + e.getMessage());
+                }
+            }
+        }
+
+        return null;
+
+    }
+
+    public void post(String sqlQuery){
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(connectionString,user,passwd);
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(sqlQuery);
+        } catch (SQLException e) {
+            System.out.println("connection error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("driver error: " + e.getMessage());
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("could not close the connection: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+}
