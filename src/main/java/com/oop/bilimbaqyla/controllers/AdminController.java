7f577534333e7daa97b3dd3711a1d28c22496a8e
Isakzend <islamkadyrbekov31@gmail.com>
2024-02-28 03:29:51 +0600

    Adminpage refactoring

--- src/main/java/com/oop/bilimbaqyla/controllers/AdminController.java
+++ src/main/java/com/oop/bilimbaqyla/controllers/AdminController.java
@@ -1,21 +1,36 @@
 package com.oop.bilimbaqyla.controllers;
 
 import com.oop.bilimbaqyla.models.Admin;
+import com.oop.bilimbaqyla.models.Parent;
+import com.oop.bilimbaqyla.repositories.AdminRepo;
+import com.oop.bilimbaqyla.repositories.ParentRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 public class AdminController {
+
     @GetMapping("/adminPage")
     public String home(HttpServletRequest request, Model model) {
-        Admin admin = (Admin) request.getSession().getAttribute("admin");
+//        Admin admin = (Admin) request.getSession().getAttribute("admin");
+//
+//        if (admin != null) {
+//            model.addAttribute("admin", admin);
+//        }
+        AdminRepo adminRepo = new AdminRepo();
+        ParentRepo parentRepo = new ParentRepo();
+        Admin admin = adminRepo.getAdminById(1);
+        ArrayList<Integer> parentsId = adminRepo.getIdOfParentsWaiting(admin.getAdminId());
+        ArrayList<Parent> parents = new ArrayList<>();
 
-        if (admin != null) {
-            model.addAttribute("admin", admin);
+        for (int id : parentsId) {
+            parents.add(parentRepo.getParentById(id));
         }
+        model.addAttribute("parents",parents);
         return "adminPage";
     }
 }
